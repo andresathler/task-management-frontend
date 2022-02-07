8ca10ff03b08a86c211d4fa51ca52bfa29a228a8
andresathler <andresathler@users.noreply.github.com>
2022-02-07 02:27:59 +0000

    deploy: ec6d45fd447b54081192f75f53bb61cc00e4a822

---
+++ precache-manifest.10e90a991da5028ae56290330fbe8b4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e73a924430d26cd43a45ba977b748a",
+    "url": "https://andresathler.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "f4d3dba4a73b4704faa6",
+    "url": "https://andresathler.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "42ca0bcaddcbdf2a58c5",
+    "url": "https://andresathler.github.io/task-management-frontend/static/js/2.a85c26d8.chunk.js"
+  },
+  {
+    "revision": "f4d3dba4a73b4704faa6",
+    "url": "https://andresathler.github.io/task-management-frontend/static/js/main.42204e01.chunk.js"
+  },
+  {
+    "revision": "39cb346522a67374329a",
+    "url": "https://andresathler.github.io/task-management-frontend/static/js/runtime~main.c794dc7f.js"
+  }
+]);
